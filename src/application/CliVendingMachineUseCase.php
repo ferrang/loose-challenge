4eb6ecfac10b8986f2c1fcb6756e185111a96f69
Ferran Gay Boelle <ferran.gayboelle@personio.de>
2024-10-08 22:06:36 +0200

    Add cli use case

---
+++ src/application/CliVendingMachineUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace application;
+
+use Domain\VendingMachine;
+
+class CliVendingMachineUseCase
+{
+    public function execute(string $command): void
+    {
+        // TODO: Fill in
+    }
+}
